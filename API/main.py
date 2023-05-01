5db9d4dcf84bf00723751edb3a5e69037ac17958
EmeryG <Emerytgriffith@gmail.com>
2023-05-01 00:46:50 -0500

    Query weather API in daily and monthly increments

--- API/main.py
+++ API/main.py
@@ -1,42 +1,73 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from datetime import date, timedelta
 from typing import List
+from io import open
+import requests, yaml, datetime
 
-app = FastAPI()
+# app = FastAPI()
 
-class Location(BaseModel):
-    locationid: str
-    locationname: str
-    state: str
-    data_start: str
-    data_end: str
+key = yaml.safe_load(open("../.key", "r"))
+url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+
+# class Location(BaseModel):
+#     locationid: str
+#     locationname: str
+#     state: str
+#     zip: str
+#     latitude: float
+#     longitude: float
     
-class Day_Weather_Entry(BaseModel):
-    locationid: str
-    high_temp: float
-    low_temp: float
-    precipitation: float
-    snow: float
-    humidity: float
-    sunshine: float
-    wind: float
+# class Day_Weather_Entry(BaseModel):
+#     locationid: str
+#     temp: float
+#     temp_max: float
+#     temp_min: float
+#     temp_feel_max: float
+#     temp_feel_min: float
+#     precipitation: float
+#     precip_type: str
+#     snow_depth: float
+#     pressure: float
+#     dewpoint: float
+#     humidity: float
+#     skycover: float
+#     windspeed: float
+#     windspeedmax: float
+#     uv_index: float
     
-class Day_Weather_Data(BaseModel):
-    date: str
-    entries: List[Day_Weather_Entry]
+# class Day_Weather_Data(BaseModel):
+#     date: date
+#     entries: List[Day_Weather_Entry]
 
-class Month(BaseModel):
-    month: str
+class Month_Year(BaseModel):
+    month: int
     year: int
     
-class Month_Weather_Data(BaseModel):
-    month: Month
-    weather_data = List[Day_Weather_Data]
+# class Month_Weather_Data(BaseModel):
+#     month_id: Month_Year
+#     weather_data = List[Day_Weather_Data]
 
-@app.get("/today", response_model=Day_Weather_Data)
-async def main():
-    return "test"
+def get_month_data(month_id: Month_Year, zip=55105):
+    first_month_day = date(month_id.year, month_id.month, 1)
+    next_month = first_month_day.replace(day=28) + timedelta(days=4)
+    last_month_day = next_month-timedelta(days=next_month.day)
+    
+    api_call = f"{url}/{zip}/{first_month_day.strftime('%Y-%m-%d')}/{last_month_day.strftime('%Y-%m-%d')}?key={key}&include=days"
 
-@app.get("/month", response_model=Month_Weather_Data)
-async def get_month_data(month: Month):
-    pass
+    return requests.get(api_call, timeout=60).json()
+
+def get_day_data(day: date, zip=55105):
+    api_call = f"{url}/{zip}/{day.strftime('%Y-%m-%d')}?key={key}&include=days"
+
+    return requests.get(api_call, timeout=60).json()
+    
+# @app.get("/today") #, response_model=Day_Weather_Data
+# async def get_today_data():
+#     return "test"
+
+# @app.get("/month") #, response_model=Month_Weather_Data
+# async def get_month_data(month_id: Month_Year):
+#     pass
+
+print(get_month_data(Month_Year(month=3, year=2023)))
