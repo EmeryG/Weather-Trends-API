b0633d06d4295de71cc14e1f438056aea5286e85
EmeryG <Emerytgriffith@gmail.com>
2023-05-02 22:42:12 -0500

    Add ability to get yesterday's weather history

--- API/main.py
+++ API/main.py
@@ -3,9 +3,8 @@ from pydantic import BaseModel, validator, root_validator
 from datetime import date, timedelta
 from typing import List
 from io import open
-import requests, yaml, datetime, os
+import requests, yaml, os
 
-os.getcwd()
 key = yaml.safe_load(open(f"{os.getcwd()}/.key", "r"))
 url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
 
@@ -69,14 +68,40 @@ class Month_Year(BaseModel):
 class Month_Weather_Data(BaseModel):
     month_id: Month_Year
     weather_data: List[Day_Weather_Entry]
-    
-    # @validator('weather_data')
-    # def weather_data_validate_obj(cls, v):
-    #     if len(v) == 0:
-    #         raise ValueError('List must not be enpty')
-    #     if v[0] is Day_Weather_Data:
-    #         return v
 
+
+def none_check(value, default_value):
+    if value == None:
+        return default_value
+    else:
+        return value
+
+def get_day_data(day: date, location: Location):
+    api_call = f"{url}/{location.city},{location.state}/{day.strftime('%Y-%m-%d')}?key={key}&include=days"
+
+    day_entry = requests.get(api_call, timeout=60).json()['days'][0]
+    
+    return Day_Weather_Entry(
+        locationid=location.locationid,
+                date=day_entry['datetime'],
+                temp_max=day_entry['tempmax'],
+                temp_min=day_entry['tempmin'],
+                temp_feel_max=day_entry['feelslikemax'],
+                temp_feel_min=day_entry['feelslikemin'],
+                precipitation=day_entry['precip'],
+                precip_type=";".join(none_check(day_entry['preciptype'], [])),
+                snow=none_check(day_entry['snow'], 0.0),
+                snow_depth=none_check(day_entry['snowdepth'], 0.0),
+                pressure=day_entry['pressure'],
+                dewpoint=day_entry['dew'],
+                humidity=day_entry['humidity'],
+                skycover=day_entry['cloudcover'],
+                winddir=day_entry['winddir'],
+                windspeed=day_entry['windspeed'],
+                windgust=none_check(day_entry['windgust'], 0.0),
+                uv_index=day_entry['uvindex']
+    )
+    
 def call_month_data(month_id: Month_Year, location: Location):
     first_month_day = date(month_id.year, month_id.month, 1)
     next_month = first_month_day.replace(day=28) + timedelta(days=4)
@@ -86,21 +111,10 @@ def call_month_data(month_id: Month_Year, location: Location):
 
     return translate_month_data(month_id, requests.get(api_call, timeout=60).json(), location)
 
-def get_day_data(day: date, location: Location):
-    api_call = f"{url}/{location.city},{location.state}/{day.strftime('%Y-%m-%d')}?key={key}&include=days"
-
-    return requests.get(api_call, timeout=60).json()
-
-def none_check(value, default_value):
-    if value == None:
-        return default_value
-    else:
-        return value
-    
-def translate_month_data(month_id, json_data, location: Location):
+def translate_month_data(month_id, json_resp, location: Location):
     trimmed_data = Month_Weather_Data(month_id=month_id, weather_data=[])
     
-    for entry in json_data['days']:
+    for entry in json_resp['days']:
         trimmed_data.weather_data.append(
             Day_Weather_Entry(
                 locationid=location.locationid,
@@ -128,9 +142,9 @@ def translate_month_data(month_id, json_data, location: Location):
     
 app = FastAPI()
 
-# @app.get("/today") #, response_model=Day_Weather_Data
-# async def get_today_data():
-#     return "test"
+@app.get("/yesterday", response_model=Day_Weather_Entry)
+async def get_yesterday_data():
+    return get_day_data(date.today()-timedelta(days=1), Location(locationid=1, city='Minneapolis', state='MN', latitude=44.9778, longitude=93.2650))
 
 @app.get("/monthly/{year}/{month}", response_model=Month_Weather_Data) #, response_model=Month_Weather_Data
 async def get_month_data(year: int, month: int): #month_id: Month_Year
